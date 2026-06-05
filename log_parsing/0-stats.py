ab1ec4b7a233030790152a961c7a71faacec6fde
OuardaHolberton <12985@holbertonstudents.com>
2026-06-05 14:33:02 +0200

    Add log parsing script

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import sys
+
+
+def print_stats(total_size, status_codes):
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+total_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
+
+try:
+    for line in sys.stdin:
+        parts = line.split()
+        try:
+            total_size += int(parts[-1])
+            code = int(parts[-2])
+            if code in status_codes:
+                status_codes[code] += 1
+        except (ValueError, IndexError):
+            pass
+        line_count += 1
+        if line_count % 10 == 0:
+            print_stats(total_size, status_codes)
+except KeyboardInterrupt:
+    print_stats(total_size, status_codes)
+    raise
